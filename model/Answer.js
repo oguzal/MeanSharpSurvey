e9f51f2b59ace381bb6754cc5c67d1c244b254ce
oguzal <oguzaltuncu@gmail.com>
2018-05-14 02:25:56 -0400

    Answer objectID

--- model/Answer.js
+++ model/Answer.js
@@ -2,8 +2,8 @@
 var mongoose = require("mongoose");
 
 var answerSchema = new mongoose.Schema({
-  questionID: { type:Object, index: true ,required:true},
-  userID:{type:Object,index:true,required:true},
+  questionID: { type: Schema.Types.ObjectId, index: true ,required:true},
+  userID:{type:Schema.Types.ObjectId,index:true,required:true},
   createdDate:{type:Date},
   updatedDate:{type:Date},
   answer:{type:String} 
